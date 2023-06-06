7c982af06d376cf3000acfa55121cd1bf90d9984
Yurii <iyureg@gmail.com>
2023-06-06 15:22:38 +0300

    ex4 классы обертки

---
+++ Lections/lesson1_javaBase_04-06-2023/examp_04_wrapperClasses/wrapper.java
@@ -0,0 +1,23 @@
+/**
+ * wrapper
+ * 
+ * классы обертки
+ * 
+ * int - Integer
+ * short - Short
+ * long	- Long
+ * byte	- Byte
+ * float - Float
+ * double - Double
+ * char	- Character
+ * boolean - Boolean
+ */
+public class wrapper {
+
+    public static void main(String[] args) {
+        int i = 123;
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.toString(i));
+    }
+}
