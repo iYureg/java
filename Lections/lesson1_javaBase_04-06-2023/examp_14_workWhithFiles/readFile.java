0e6e8f844b03ae91c3b9f4355ed83408b29fced2
Yurii <iyureg@gmail.com>
2023-06-07 00:42:40 +0300

    ex14 работа с файлами

---
+++ Lections/lesson1_javaBase_04-06-2023/examp_14_workWhithFiles/readFile.java
@@ -0,0 +1,21 @@
+/*
+ * readFile
+ * 
+ * чтение файла
+ */
+import java.io.*;
+public class readFile {
+    public static void main(String[] args) throws Exception {
+        FileReader fr = new FileReader("file.txt");
+        int c;
+        while ((c = fr.read()) != -1) {
+            char ch = (char) c;
+            if (ch == '\n') {
+                System.out.print(ch);
+            } else {
+                System.out.print(ch);
+            }
+        }
+        fr.close();
+    } 
+}
