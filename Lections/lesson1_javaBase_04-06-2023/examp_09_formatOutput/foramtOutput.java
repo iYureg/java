e57895bf41bab094f507063f7727a9aa8f6b21bf
Yurii <iyureg@gmail.com>
2023-06-06 20:33:42 +0300

    ex9 форматированный вывод

---
+++ Lections/lesson1_javaBase_04-06-2023/examp_09_formatOutput/foramtOutput.java
@@ -0,0 +1,23 @@
+/**
+ * foramtOutput
+ * 
+ * форматированный вывод
+ */
+public class foramtOutput {
+
+    public static void main(String[] args) {
+        int a = 1, b = 2;
+        int c = a + b;
+        String res = String.format("%d + %d = %d \n", a, b, c); // если куда-то хотим положить новую строку
+        System.out.printf("%d + %d = %d \n", a, b, c);
+        System.out.println(res);
+
+        float pi = 3.1415f;
+        System.out.printf("%f\n", pi); // 3,141500
+        System.out.printf("%.2f\n", pi); // 3,14
+        System.out.printf("%.3f\n", pi); // 3,141
+        System.out.printf("%e\n", pi); // 3,141500e+00
+        System.out.printf("%.2e\n", pi); // 3,14e+00   
+        System.out.printf("%.3e\n", pi); // 3,141e+00
+    }
+}
